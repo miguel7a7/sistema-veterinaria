ecab5a6e451aefbea8404cb4de45f201c7862933
webmaick <webmaick@gmail.com>
2021-09-11 16:19:21 -0400

    Preparacion inicial del proyectoo

---
+++ src/AppVeterinaria.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AppVeterinaria = () => {
+	return (
+		<div className="container-fluid p-0">
+			<h2 className="d-inline box_shadow">Bienvenido</h2>
+		</div>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./sass/custom.scss";
+import reportWebVitals from "./reportWebVitals";
+import { AppVeterinaria } from "./AppVeterinaria";
+import "../node_modules/bootstrap/dist/js/bootstrap.min";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<AppVeterinaria />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
